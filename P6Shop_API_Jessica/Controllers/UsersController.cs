8a22ad9de29706c3cda666aa15071de84eafbaa9
JessicaYFR <jessicafr101995@gmail.com>
2022-11-25 10:45:49 -0600

    ñññ

--- P6Shop_API_Jessica/Controllers/UsersController.cs
+++ P6Shop_API_Jessica/Controllers/UsersController.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using P6Shop_API_Jessica.Attributes;
 using P6Shop_API_Jessica.Models;
+using P6Shop_API_Jessica.Models.DTOs;
 
 namespace P6Shop_API_Jessica.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [ApiKey]
+   // [ApiKey]
     public class UsersController : ControllerBase
     {
         private readonly P6SHOPPINGContext _context;
@@ -32,19 +33,58 @@ namespace P6Shop_API_Jessica.Controllers
             return await _context.Users.ToListAsync();
         }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
+        // GET: api/Users/GetUserInfo?email=@gmail.com
+        [HttpGet("GetUserInfo")]
         
-        public async Task<ActionResult<User>> GetUser(int id)
+        public ActionResult<IEnumerable<UserDTO>> GetUserInfo(string email)
         {
-            var user = await _context.Users.FindAsync(id);
+            //las consultas linq se parecen mucho a las normales que hemos hecho en T-sql
+//una de las diferencias es que podemos usar una "tabla temporal" para almacenar los resultaos y luego usarla para llenar los atributos de un modelo o DTO
+            var query = (from u in _context.Users
+                         join r in _context.UserRoles on
+                         u.IduserRole equals r.IduserRole
+                         join c in _context.Countries
+                         on u.Idcountry equals c.Idcountry
+                         where u.Email == email && u.Active == true
+                         select new
+                         {
+                             idusuario = u.Iduser,
+                             nombre = u.Name,
+                             email = u.Email,
+                             correorespaldo = u.BackUpEmail,
+                             telefono = u.PhoneNumber,
+                             idrol = r.IduserRole,
+                             idpais = c.Idcountry,
+                             roldesc = r.UserRoleDescription,
+                             pais = c.CountryName }).ToList();
 
-            if (user == null)
+            List<UserDTO> list = new List<UserDTO>();
+
+            foreach (var item in query)
+            {
+                UserDTO NewItem = new UserDTO();
+
+                NewItem.IDUsuario = item.idusuario;
+                NewItem.Nombre = item.nombre;
+                NewItem.CorreoElectronico = item.email;
+                NewItem.CorreoRespaldo = item.correorespaldo;
+                NewItem.MuneroTelefono = item.telefono;
+                NewItem.IDRol = item.idrol;
+                NewItem.IDPais = item.idpais;
+                NewItem.RolDescripcion = item.roldesc;
+                NewItem.PaisNombre = item.pais;
+
+                list.Add(NewItem);
+            }
+
+          
+
+            if (list == null)
             {
                 return NotFound();
             }
 
-            return user;
+            return list;
         }
         // GET: api/Users/ValidateLogin?UserName=A&UserPassword=1
         [HttpGet("ValidateLogin")]
---
+++ P6Shop_API_Jessica/Models/DTOs/UserDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace P6Shop_API_Jessica.Models.DTOs
+{
+    public class UserDTO
+    {
+        //Un DTO (Data Transfer Object) es el método nás usado para
+        //pasar estructuras entre un API y una App (o cualquier otro UI)
+        //Se hace principalemte por dos razones:
+        //1. para que el equipo de desarrollo del UI (o posibles ingenierías inversas)
+        //no entiendan la estructura de los models en el API
+        //2. Simplificar objetos muy complejos, ya que es más rápido pasar la mínima data requerida por los objetos json entre el app y el API
+        //3. Si es necesario volver a generar los modelos con el scaffold porque hubo algún cambio de estructura de una 
+        //tabla por ejemplo, no es necesario cambiar nada en el controller si la estructura del modelo
+        //cambió y además habíamos hecho cambios en el modelo generado automáticamente
+
+        //Nota: Para efectos de ejemplificar el uso del DTO, Vamos a manejar los nombres 
+        //de propiedades en español.
+
+        public int IDUsuario { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string CorreoElectronico { get; set; } = null!;
+        public string Contrasennia { get; set; } = null!;
+        public string CorreoRespaldo { get; set; } = null!;
+        public string MuneroTelefono { get; set; } = null!;
+        public int IDRol { get; set; }
+        public int IDPais { get; set; }
+
+        //No es necesario en todos los casos escribir las composiciones, este es uno de esos casos
+
+        public string RolDescripcion { get; set; } = null;
+        public string PaisNombre { get; set; } = null;
+
+    }
+}
